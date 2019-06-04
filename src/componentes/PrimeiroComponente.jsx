9e47ce217b9503c0f0ea45acd25b63d18c38f67e
daniloalves-luizalabs <danilo.alves@luizalabs.com>
2019-06-04 13:17:13 -0300

    Novas Aulas

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-function primeiro (){
-    return <h1>Primeiro Componente!</h1>
-}
+export default (props) => 
+    <h1>{props.valor}</h1>
 
-export default primeiro
+
+
+
+
+//  export default () => 
+//     <h1>Primeiro Componente!</h1>
+   
+
+// function primeiro (){
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+//export default primeiro
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import PrimeiroComponente from './componentes/PrimeiroComponente'
+//import PrimeiroComponente from './componentes/PrimeiroComponente'
+import {CompA, CompB} from './componentes/DoisComponentes'
 
 
 const elemento = document.getElementById('root')
 
 ReactDom.render(
     <div>
-        <PrimeiroComponente></PrimeiroComponente>
+        <CompA valor="Olá eu sou A"></CompA>
+        <CompB valor="Olá eu sou B"></CompB>
+        {/* <PrimeiroComponente valor="Bom dia"></PrimeiroComponente> */}
     </div>
 ,elemento)
 
