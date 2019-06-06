1900b4ae6866ad51b78af7000f332049c2b4f269
daniloalves-luizalabs <danilo.alves@luizalabs.com>
2019-06-06 17:06:33 -0300

    Cont..

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react'
 import ReactDom from 'react-dom'
 //import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import {CompA, CompB} from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
-
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 
 ReactDom.render(
     <div>
-        <MultiElementos/>
+        <Familia sobrenome="Morais">
+            <Membro nome="Danilo"></Membro>
+            <Membro nome="Danilo1"></Membro>
+        </Familia>
+        {/* <FamiliaSilva/> */}
+        {/* <MultiElementos/> */}
         {/* <CompA valor="Olá eu sou A"></CompA>
         <CompB valor="Olá eu sou B"></CompB> */}
         {/* <PrimeiroComponente valor="Bom dia"></PrimeiroComponente> */}
